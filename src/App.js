9f5fdcd4a7978e9192dd8969cb30a4b6a785e7a1
Rajat Soni <rajatsoni@Rajats-MacBook-Pro.local>
2024-10-14 14:51:24 +0530

    hard coded to show product on the screen

--- src/App.js
+++ src/App.js
@@ -1,10 +1,53 @@
-
-import './App.css';
+import "./App.css";
 
 function App() {
+
+  const productsArr = [
+    {
+      title: "Colors",
+
+      price: 100,
+
+      imageUrl:
+        "https://prasadyash2411.github.io/ecom-website/img/Album%201.png",
+    },
+
+    {
+      title: "Black and white Colors",
+
+      price: 50,
+
+      imageUrl:
+        "https://prasadyash2411.github.io/ecom-website/img/Album%202.png",
+    },
+
+    {
+      title: "Yellow and Black Colors",
+
+      price: 70,
+
+      imageUrl:
+        "https://prasadyash2411.github.io/ecom-website/img/Album%203.png",
+    },
+
+    {
+      title: "Blue Color",
+
+      price: 100,
+
+      imageUrl:
+        "https://prasadyash2411.github.io/ecom-website/img/Album%204.png",
+    },
+  ];
   return (
     <div className="App">
-      <h1>working</h1>
+      {productsArr.map((product, index) => (
+        <div key = {index}>
+          <h3>{product.title}</h3>
+          <img src={product.imageUrl}/>
+          <p>{product.price}</p>
+        </div>
+      ))}
     </div>
   );
 }
